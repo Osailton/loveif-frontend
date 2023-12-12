a44746a743b88de010a5932b793666b4ecae549c
osailton <osailtonvieira@gmail.com>
2023-12-11 23:16:04 -0300

    Added Router; Created the footer and navbar; Defined simple css for the components.

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+// CSS
+import styles from './Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <h3>Um projeto do IFRN Campus Lajes</h3>
+            <p>Com Amor IF &copy; 2023</p>
+        </footer>
+    );
+}
+
+export default Footer;
